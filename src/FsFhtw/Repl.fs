095486d602be49fdca8153429c75678628989ab5
sl1vr <lautner.f@gmail.com>
2022-01-04 19:12:15 +0100

    es geht...a bisserl

--- src/FsFhtw/Repl.fs
+++ src/FsFhtw/Repl.fs
@@ -28,11 +28,52 @@ let createHelpText () : string =
 
 open Commands
 
+
+
+open User
+open Document
+open Priority
+
+let CreateDocument () : Document =
+    let from = CurrentUser.Instance.GetUsername()
+    let ``to`` = {FirstName = "Herbert"; LastName = "Emmentaler"}
+    let header = {From = from; To = ``to``; Date = DateTime.Today}
+    
+    let taxInspection = nameof(TaxInspection)
+    let constructionSiteSupervision = nameof(ConstructionSiteSupervision)
+    
+    printf """%s or %s""" taxInspection constructionSiteSupervision
+    let input = Console.ReadLine()
+    let body = 
+        if input = taxInspection then taxInspection
+        elif input = constructionSiteSupervision then constructionSiteSupervision
+        else failwith "Invalid Template"
+    
+    let content = {Header = header; Body = body}
+    
+    printf "Priority?"
+    let input2 = Console.ReadLine()
+    let priority = 
+        if input2 = nameof(High) then Priority.High
+        elif input2 = nameof(Normal) then Priority.Normal
+        elif input2 = nameof(Low) then Priority.Low
+        else failwith "Invalid Priority"
+    
+    let metaData = {AccessLevel = CurrentUser.Instance.GetAccessLevel();Department = CurrentUser.Instance.GetDepartment();Priority = priority}
+
+    printf "File name?"
+    let name = Console.ReadLine()
+
+    { Name = name; MetaData = metaData; Content = content}
+
+    
+
+
 let evaluateCommands (command : Command) =
     match command with
     | Command.CreateDocument -> 
         //(command, sprintf "CreateDocument")
-        DocumentLogic.CreateDocument()
+        CreateDocument()
         (command, sprintf "Document created! Probably...")
     | Command.ReadDocument -> 
         (command, sprintf "ReadDocument")
@@ -66,3 +107,6 @@ let rec loop (cmd : Command) =
     |> evaluate cmd
     |> print
     |> loop
+
+
+

ecdf446e9d112dea061b9497c1bdd1e94510882d
Jakob Müllner <dalistige@gmail.com>
2022-01-05 18:43:49 +0100

    Update documentLogic.fs to contain the document logic

--- src/FsFhtw/Logic/documentLogic.fs
+++ src/FsFhtw/Logic/documentLogic.fs
@@ -1,1 +1,39 @@
 module DocumentLogic
+
+open System
+
+open User
+open Document
+open Priority
+
+let createDocument () : Document =
+    let from = CurrentUser.Instance.GetUsername()
+    let ``to`` = {FirstName = "Herbert"; LastName = "Emmentaler"}
+    let header = {From = from; To = ``to``; Date = DateTime.Today}
+
+    let taxInspection = nameof(TaxInspection)
+    let constructionSiteSupervision = nameof(ConstructionSiteSupervision)
+
+    printf """%s or %s""" taxInspection constructionSiteSupervision
+    let input = Console.ReadLine()
+    let body =
+        if input = taxInspection then taxInspection
+        elif input = constructionSiteSupervision then constructionSiteSupervision
+        else failwith "Invalid Template"
+
+    let content = {Header = header; Body = body}
+
+    printf "Priority?"
+    let input2 = Console.ReadLine()
+    let priority =
+        if input2 = nameof(High) then High
+        elif input2 = nameof(Normal) then Normal
+        elif input2 = nameof(Low) then Low
+        else failwith "Invalid Priority"
+
+    let metaData = {AccessLevel = CurrentUser.Instance.GetAccessLevel();Department = CurrentUser.Instance.GetDepartment();Priority = priority}
+
+    printf "File name?"
+    let name = Console.ReadLine()
+
+    { Name = name; MetaData = metaData; Content = content}
--- src/FsFhtw/Repl.fs
+++ src/FsFhtw/Repl.fs
@@ -17,7 +17,7 @@ let read (input : string) =
     | SwitchUser -> Commands.SwitchUser |> Command
     | Help -> Message.HelpRequested
     | ParseFailed -> NotParsable input
-    
+
 open Microsoft.FSharp.Reflection
 
 let createHelpText () : string =
@@ -27,63 +27,23 @@ let createHelpText () : string =
     |> (fun s -> s.Trim() |> sprintf "Known commands are: %s")
 
 open Commands
-
-
-
-open User
-open Document
-open Priority
-
-let CreateDocument () : Document =
-    let from = CurrentUser.Instance.GetUsername()
-    let ``to`` = {FirstName = "Herbert"; LastName = "Emmentaler"}
-    let header = {From = from; To = ``to``; Date = DateTime.Today}
-    
-    let taxInspection = nameof(TaxInspection)
-    let constructionSiteSupervision = nameof(ConstructionSiteSupervision)
-    
-    printf """%s or %s""" taxInspection constructionSiteSupervision
-    let input = Console.ReadLine()
-    let body = 
-        if input = taxInspection then taxInspection
-        elif input = constructionSiteSupervision then constructionSiteSupervision
-        else failwith "Invalid Template"
-    
-    let content = {Header = header; Body = body}
-    
-    printf "Priority?"
-    let input2 = Console.ReadLine()
-    let priority = 
-        if input2 = nameof(High) then Priority.High
-        elif input2 = nameof(Normal) then Priority.Normal
-        elif input2 = nameof(Low) then Priority.Low
-        else failwith "Invalid Priority"
-    
-    let metaData = {AccessLevel = CurrentUser.Instance.GetAccessLevel();Department = CurrentUser.Instance.GetDepartment();Priority = priority}
-
-    printf "File name?"
-    let name = Console.ReadLine()
-
-    { Name = name; MetaData = metaData; Content = content}
-
-    
-
+open DocumentLogic
 
 let evaluateCommands (command : Command) =
     match command with
-    | Command.CreateDocument -> 
+    | Command.CreateDocument ->
         //(command, sprintf "CreateDocument")
-        CreateDocument()
+        createDocument()
         (command, sprintf "Document created! Probably...")
-    | Command.ReadDocument -> 
+    | Command.ReadDocument ->
         (command, sprintf "ReadDocument")
-    | Command.UpdateDocument -> 
+    | Command.UpdateDocument ->
         (command, sprintf "UpdateDocument")
-    | Command.DisplayAllDocuments -> 
+    | Command.DisplayAllDocuments ->
         (command, sprintf "DisplayAllDocuments")
-    | Command.CreateUser -> 
+    | Command.CreateUser ->
         (command, sprintf "CreateUser")
-    | Command.SwitchUser -> 
+    | Command.SwitchUser ->
         (command, sprintf "SwitchUser")
 
 let evaluate (command : Command) (msg : Message) =
